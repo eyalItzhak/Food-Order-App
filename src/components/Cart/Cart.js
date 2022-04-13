4b41437cec21314be5da6f281790ef2922a48668
eyalItzhak <62293316+eyalItzhak@users.noreply.github.com>
2022-04-13 23:53:49 +0300

    updated

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,14 @@
-import { useContext } from 'react';
+import { useContext, useState } from "react";
 
-import Modal from '../UI/Modal';
-import CartItem from './CartItem';
-import classes from './Cart.module.css';
-import CartContext from '../../store/cart-context';
+import Modal from "../UI/Modal";
+import CartItem from "./CartItem";
+import classes from "./Cart.module.css";
+import CartContext from "../../store/cart-context";
+
+import Checkout from "./Checkout";
 
 const Cart = (props) => {
+  const [isCheckOut, setIsCheckOut] = useState(false);
   const cartCtx = useContext(CartContext);
 
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
@@ -16,11 +19,22 @@ const Cart = (props) => {
   };
 
   const cartItemAddHandler = (item) => {
-    cartCtx.addItem({ ...item, amount: 1 });
+    cartCtx.addItem(item);
   };
 
+  const OrderHandler = () => {
+    setIsCheckOut(true);
+  };
+
+  const submitOrderHandler = (userData)=>{
+    fetch('https://react-udemy-305c5-default-rtdb.europe-west1.firebasedatabase.app/orders.json',{method:'post', body:JSON.stringify({
+      user :userData,
+      orderItems:cartCtx.items
+    })})
+  }
+
   const cartItems = (
-    <ul className={classes['cart-items']}>
+    <ul className={classes["cart-items"]}>
       {cartCtx.items.map((item) => (
         <CartItem
           key={item.id}
@@ -34,6 +48,19 @@ const Cart = (props) => {
     </ul>
   );
 
+  const modelAction = (
+    <div className={classes.actions}>
+      <button className={classes["button--alt"]} onClick={props.onClose}>
+        Close
+      </button>
+      {hasItems && (
+        <button className={classes.button} on onClick={OrderHandler}>
+          Order
+        </button>
+      )}
+    </div>
+  );
+
   return (
     <Modal onClose={props.onClose}>
       {cartItems}
@@ -41,12 +68,9 @@ const Cart = (props) => {
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
-      <div className={classes.actions}>
-        <button className={classes['button--alt']} onClick={props.onClose}>
-          Close
-        </button>
-        {hasItems && <button className={classes.button}>Order</button>}
-      </div>
+
+      {isCheckOut && <Checkout onConfirm={submitOrderHandler} onCancel={props.onClose} />}
+      {!isCheckOut && modelAction}
     </Modal>
   );
 };
--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,33 +1,61 @@
-import { useEffect,useState } from 'react';
+import { useEffect, useState } from "react";
 
-import Card from '../UI/Card';
-import MealItem from './MealItem/MealItem';
-import classes from './AvailableMeals.module.css';
-
-
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
+import classes from "./AvailableMeals.module.css";
 
 const AvailableMeals = () => {
-  const [meals,setMeals] = useState([]);
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
 
-  useEffect(()=>{
-    const fetchMeals = async () =>{
-      const response =await fetch("https://react-udemy-305c5-default-rtdb.europe-west1.firebasedatabase.app/meals.json")
-      const responseData =  await response.json();
+  useEffect(() => {
+    const fetchMeals = async () => {
+      setIsLoading(true);
+      const response = await fetch(
+        "https://react-udemy-305c5-default-rtdb.europe-west1.firebasedatabase.app/meals.json"
+      );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
+      const responseData = await response.json();
       const loadedMeals = [];
 
-      for(const key in responseData){
+      for (const key in responseData) {
         loadedMeals.push({
-          id:key,
+          id: key,
           name: responseData[key].name,
           description: responseData[key].description,
           price: responseData[key].price,
-        })
+        });
       }
-      setMeals(loadedMeals)
+      setMeals(loadedMeals);
+      setIsLoading(false);
     };
-    fetchMeals();
-  },[])
-  
+
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
+  }, []);
+
+  if (httpError) {
+    return (
+      <section className={classes.mealError}>
+        <p>{httpError}...</p>
+      </section>
+    );
+  }
+
+  if (isLoading) {
+    return (
+      <section className={classes.mealLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
 
   const mealsList = meals.map((meal) => (
     <MealItem
